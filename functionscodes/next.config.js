456b6874a9027db1177cb94babcd1c9a83811b21
Natthanarong Tiangjit <natthanarong.tian@bumail.net>
2025-09-13 08:33:03 +0700

    Revert "Update next.config.js"
    This reverts commit 7e4b1b36a1269d78dd82c8650557c1232351419f.

--- functionscodes/next.config.js
+++ functionscodes/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    allowedDevOrigins: ["*"]
+    allowedDevOrigins: [
+      'localhost:3000',
+      '127.0.0.1:3000',
+      '0.0.0.0:3000',
+      '10.232.87.23:3000'
+    ]
   }
 }
 
